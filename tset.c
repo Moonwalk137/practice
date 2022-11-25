ca2f665998cdd3965f07d1fe9b010456d602a488
Moonwalk137 <118429105+Moonwalk137@users.noreply.github.com>
2022-11-25 17:56:23 +0800

    Add files via upload

---
+++ tset.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	c = (a / 100 * 60) + a % 100 + b;		//输入的时间转换为分钟，并进行加减
+	if (c > 1440) {							//判断时间前进是否大于一天（24*60=1440）
+		d = c / 1440;
+		c = c % 1440;						//取余后得到前进时间的分钟数（已去除前进天数）
+		c = c / 60 * 100 + c % 60;			//从分钟再转换为四位数时间的表示
+		printf("时间为%d天后的", d);
+		printf("%03d", c);
+}
+	else if (c > 0 && c < 1440)
+	{
+		c = c / 60 * 100 + c % 60;
+		printf("%03d", c);
+	}
+	else if (c > -1440 && c < 0)
+	{
+		c = 24 * 60 + c;
+		c = c / 60 * 100 + c % 60;
+		printf("%03d", c);
+	}
+	else 
+	{
+		d = c / 1440;
+		c = c % 1440;
+		c = 24 * 60 + c;
+		c = c / 60 * 100 + c % 60;
+		printf("时间为%d天前的", d);
+		printf("%03d", c);
+	}
+	return 0;
+}
+
